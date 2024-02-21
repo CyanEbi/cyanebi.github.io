a16f7d0cf9efa9a0b44af2e40682e3bdfeca2893
CyanEbi <cyanebi@proton.me>
2024-02-21 12:40:39 +0100

    Cooler css

--- populate_grid.js
+++ populate_grid.js
@@ -3,23 +3,28 @@ populateGrid();
 async function populateGrid() {
     const response = await fetch("item_list.json");
     const list = await response.json();
+    
+    const item_grid = document.getElementById("item_grid");
 
     for (const item in list) {
         await fetch(`Items/${list[item]}.json`)
             .then(response => response.json())
-            .then(data => createItem(data));
+            .then(data => addItem(item_grid, data));
     }
 }
 
-function createItem(data) {
+function addItem(item_grid, data) {
     const item = document.createElement("div");
+    item.className = "item_container";
     item.innerHTML = `
-    <img src="${data.image}">
-    <h3>${data.english}</h3>
-    <p>${data.romaji}</p>
-    <p>${data.japanese}</p>
-    <a href="${data.mal}" target="_blank">MAL</a>
-    <a href="${data.anilist}" target="_blank">Anilist</a>
+    <div class="item">
+        <img src="${data.image}" height=350px>
+        <h3>${data.english}</h3>
+        <p>${data.romaji}</p>
+        <p>${data.japanese}</p>
+        <a href="${data.mal}" target="_blank">MAL</a>
+        <a href="${data.anilist}" target="_blank">Anilist</a>
+    </div>
     `
-    document.getElementById("item_grid").appendChild(item);
+    item_grid.appendChild(item);
 }
