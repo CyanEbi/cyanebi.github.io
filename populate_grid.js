23d74dbf5f64868f14ca97710e7cf6b2d2d1738d
CyanEbi <cyanebi@proton.me>
2024-02-24 00:28:33 +0100

    Different attempt at improving image loading speed

--- populate_grid.js
+++ populate_grid.js
@@ -5,21 +5,33 @@ async function populateGrid() {
     const list = await response.json();
     
     const item_grid = document.getElementById("item_grid");
-
-    for (const item in list) {
-        await fetch(`Items/${list[item]}.json`)
+    
+    const len = list.length;
+    const item_list = Array(len);
+    const promise_list = Array(len);
+    for (let i = 0; i < len; i++) {
+        promise_list[i] = fetch(`Items/${list[i]}.json`)
             .then(response => response.json())
-            .then(data => addItem(item_grid, data));
+            .then(data => item_list[i] = createItem(item_grid, data));
+    }
+
+    
+    await Promise.all(promise_list);
+    console.log(item_list);
+
+    for (let i = 0; i < len; i++) {
+        console.log(item_list[i]);
+        item_grid.appendChild(item_list[i]);
     }
 }
 
-function addItem(item_grid, data) {
+function createItem(item_grid, data) {
     const item = document.createElement("div");
     item.className = "item_container";
     item.innerHTML = `
     <div class="item">
         <div class="image_container" style="cursor: pointer;" onclick='window.location="${data.download}";'>
-            <img src="${data.image}" loading="lazy">
+            <img src="${data.image}">
         </div>
         <h3>${data.english}</h3>
         <p>${data.romaji}</p>
@@ -30,5 +42,5 @@ function addItem(item_grid, data) {
         </div>
     </div>
     `
-    item_grid.appendChild(item);
+    return item;
 }
